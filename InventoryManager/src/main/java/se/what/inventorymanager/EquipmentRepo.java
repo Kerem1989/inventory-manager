21550c8df48762a19075c394c177e793aa1655a8
Magnus Nording <magnus.nording@gmail.com>
2023-12-19 18:27:28 +0100

    EquipmentRepo skapat, findBy...name, type, purchaseDate, purchasedBy

---
+++ InventoryManager/src/main/java/se/what/inventorymanager/EquipmentRepo.java
@@ -0,0 +1,15 @@
+package se.what.inventorymanager;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface EquipmentRepo extends JpaRepository<Equipment, Integer> {
+    List<Equipment> findByName(String name);
+    List<Equipment> findByType(EquipmentType type);
+    List<Equipment> findByPurchaseDate(Date date);
+    List<Equipment> findByPurchasedBy(int id);
+}
