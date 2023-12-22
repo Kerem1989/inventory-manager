06abfa2b2d3d2c14db7a9f460da2be130f566219
Dmitry Khalizov <136365505+DmitryKhalizov@users.noreply.github.com>
2023-12-22 09:16:54 +0100

    Jag skapade Equipment Status class, repo, service class där metoder ska ligga samt en class med Enum status. 22 december 2023

--- InventoryManager/src/main/java/se/what/inventorymanager/EquipmentSupportService.java
+++ InventoryManager/src/main/java/se/what/inventorymanager/EquipmentSupportService.java
@@ -8,11 +8,11 @@ public class EquipmentSupportService {
     @Autowired
     EquipmentSupportRepo equipmentSupportRepo;
 
-    public EquipmentSupport addEquipmentSupport(int equipment_id, EquipmentStatus status,
+  /*  public EquipmentSupport addEquipmentSupport(int equipment_id, EquipmentStatus status,
                                                 int support_record) {
         EquipmentSupport equipmentSupport = new EquipmentSupport(equipment_id, status, support_record);
         return equipmentSupportRepo.save(equipmentSupport);
-    }
+   */ }
 
 
 }
